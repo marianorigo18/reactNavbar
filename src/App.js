23eec89fb2e81867f25a44ca93da01fd4db46b0a
marianorigo18 <marianowagnerrigo@gmail.com>
2023-03-08 23:03:11 -0300

    fix commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
+import "./App.css" 
 import Navbar from "./components/Navbar"
 const App = () => {
     return (
         <div>
             <Navbar/>
-            <h1>Hello</h1>
-            <h2>Hello from </h2>
         </div>
         )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
     return (
-        <h1>Navbar from navbar component</h1>
+        <h1>Este proyecto tendra las variantes de card</h1>
     )
 }
 export default Navbar;
