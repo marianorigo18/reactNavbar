2c5090f4aee5705b24f15e04e06041ea702211fd
marianorigo18 <marianowagnerrigo@gmail.com>
2023-03-09 23:30:56 -0300

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,51 +1,23 @@
-import styled from "styled-components";
-
-const Header = styled.header`
-background-color: #454545;
-position: fixed;
-width: 100%;
-`
-const NavContainer = styled.div`
-display: flex;
-justify-content: space-between;
-align-items: center;
-max-width: 1200px;
-width: 90%;
-margin: auto;
-height: 80px;
-`
-
-const NavItems = styled.div`
-display: flex;
-`
-
-const NavItem = styled.li`
-margin-left: 10px;
-`
-
-const NavIcons = styled.div`
-display: none;
-position: fixed;
-bottom: 20px;
-right: 20px;
-z-index: 999;
-`
-
-const NavIcon = styled.div`
-transition: .3s;
-font-size: 40px;
-cursor: pointer;
-color:  #454545;
-`
+import "./Navbar.css"
 const Navbar = () => {
     return (
-        <Header>
-            <NavContainer>
-                <NavItems>
-                    <NavItem>hello</NavItem>
-                </NavItems>
-            </NavContainer>
-        </Header>
+        <header className="header">
+        <nav className="nav__container">
+            <h2 className="nav__logo">Logo</h2>
+            <ul className="nav__navbar" id="nav__navbar">
+                <div className="nav__items">
+                    <li className="nav__item"><a href="#" className="nav__link">link</a></li>
+                    <li className="nav__item"><a href="#" className="nav__link">link</a></li>
+                    <li className="nav__item"><a href="#" className="nav__link">link</a></li>
+                    <li className="nav__item"><a href="#" className="nav__link">link</a></li>
+                </div>
+            </ul>
+            <div className="nav__icons" id="nav__icons">
+                <div className="nav__close nav__icon" id="nav__close"><i className='bx bx-x'></i></div>
+                <div className="nav__menu nav__icon" id="nav__menu"><i className='bx bx-menu-alt-right'></i></div>
+            </div>
+        </nav>
+        </header>
     )
 }
 export default Navbar;
