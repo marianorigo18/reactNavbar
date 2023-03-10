cb538298e80b7f996bac65acaa0f5b1deefab310
marianorigo18 <marianowagnerrigo@gmail.com>
2023-03-10 10:28:27 -0300

    fix commit

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,11 +4,13 @@ const Likes = () => {
 
     const [likes, setLikes] = useState(0);
     const [quantity, setQuantity] = useState(0);
-    return(<div>
-            <span>likes{likes}</span>
-            <span>quantity{quantity}</span>
-            <button style={{marginTop: "200px"}} onClick={() => {setLikes(likes - 1); setQuantity(quantity + 1)}}>-likes</button>
-            <button style={{marginTop: "200px"}} onClick={() => {setLikes(likes + 1); setQuantity(quantity + 1)}}>+likes</button>
+    return(<div style={{paddingTop: "200px", display: "flex", flexDirection: "column", alignItems: "center"}}>
+            <span>likes || {likes}</span>
+            <span>cantidad de veces que usaste los botones || {quantity}</span>
+            <div>
+                <button onClick={() => {setLikes(likes - 1); setQuantity(quantity + 1)}}>-likes</button>
+                <button onClick={() => {setLikes(likes + 1); setQuantity(quantity + 1)}}>+likes</button>
+            </div>
         </div>)
 }
 export default Likes;
