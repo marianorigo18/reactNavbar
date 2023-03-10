1db0f5fcec2266eb9fddcd3b5f4e5cba4d7f263d
marianorigo18 <marianowagnerrigo@gmail.com>
2023-03-10 10:23:38 -0300

    fix commit menu

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 
 const Likes = () => {
 
-    const [likes, setLikes] = useState(0)
+    const [likes, setLikes] = useState(0);
+    const [quantity, setQuantity] = useState(0);
     return(<div>
-            <span>{likes}</span>
-            <button style={{marginTop: "200px"}} onClick={() => {setLikes(likes + 1)}}>+likes</button>
-            <button style={{marginTop: "200px"}} onClick={() => {setLikes(likes - 1)}}>-likes</button>
+            <span>likes{likes}</span>
+            <span>quantity{quantity}</span>
+            <button style={{marginTop: "200px"}} onClick={() => {setLikes(likes - 1); setQuantity(quantity + 1)}}>-likes</button>
+            <button style={{marginTop: "200px"}} onClick={() => {setLikes(likes + 1); setQuantity(quantity + 1)}}>+likes</button>
         </div>)
 }
 export default Likes;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,15 +6,15 @@ const Navbar = () => {
             <h2 className="nav__logo">Logo</h2>
             <ul className="nav__navbar" id="nav__navbar">
                 <div className="nav__items">
-                    <li className="nav__item"><button className="nav__link">link</button></li>
-                    <li className="nav__item"><button className="nav__link">link</button></li>
-                    <li className="nav__item"><button className="nav__link">link</button></li>
-                    <li className="nav__item"><button className="nav__link">link</button></li>
+                    <li className="nav__item"><a href="index.html" className="nav__link">link</a></li>
+                    <li className="nav__item"><a href="index.html" className="nav__link">link</a></li>
+                    <li className="nav__item"><a href="index.html" className="nav__link">link</a></li>
+                    <li className="nav__item"><a href="index.html" className="nav__link">link</a></li>
                 </div>
             </ul>
             <div className="nav__icons" id="nav__icons">
                 <div className="nav__close nav__icon" id="nav__close"><i className='bx bx-x'></i></div>
-                <div className="nav__menu nav__icon" id="nav__menu"><i className='bx bx-menu-alt-right'></i></div>
+                <div onClick={(e)=> {e.target.parentElement.parentElement.parentElement.children[1].classList.toggle("menu__visivility")}} className="nav__menu nav__icon" id="nav__menu"><i className='bx bx-menu-alt-right'></i></div>
             </div>
         </nav>
         </header>
